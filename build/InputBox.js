0b14c12f87a2e8695a03096a0996213ec1cc9168
Rico Huijbers <rix0rrr@gmail.com>
2014-07-21 22:08:06 +0200

    Add built version for easy cloning

---
+++ build/InputBox.js
@@ -0,0 +1,75 @@
+/** @jsx React.DOM */
+var InputBox = React.createClass({displayName: 'InputBox',
+    getInitialState: function() {
+        return { text: '', autocompletes: [] };
+    },
+    componentDidMount: function() {
+        Events.register('autocomplete:results', function(ev) {
+            this.setState({ autocompletes: ev.results });
+        }.bind(this));
+    },
+    handleSubmit: function(ev) {
+        Events.fire('input:add', { text: this.state.text });
+        this.setState({ text: '', autocompletes: [] });
+
+        ev.preventDefault();
+        ev.stopPropagation();
+    },
+    handleChange: function(ev) {
+        this.setState({ text: ev.target.value });
+    },
+    fireOne: function(text, ev) {
+        Events.fire('input:add', { text: text });
+        this.setState({ text: '', autocompletes: [] });
+
+        ev.preventDefault();
+        ev.stopPropagation();
+    },
+    handleKeyUp: function(ev) {
+        Events.fire('autocomplete:search', { q: ev.target.value });
+    },
+    handleBlur: function(ev) {
+        window.setTimeout(function() {
+            this.setState({ autocompletes: [] })
+        }.bind(this), 100);
+    },
+    render: function() {
+        return (
+            React.DOM.form( {onSubmit:this.handleSubmit}, 
+                React.DOM.div( {className:"row collapse"}, 
+                    React.DOM.div( {className:"small-10 columns", style:{ position: 'relative' }}, 
+                        React.DOM.input( {key:"input",
+                               ref:"inputBox",
+                               type:"text",
+                               style:{ display: 'block' },
+                               value: this.state.text, 
+                               onChange:this.handleChange,
+                               onFocus:this.handleKeyUp,
+                               onKeyUp:this.handleKeyUp,
+                               onBlur:this.handleBlur}
+                               ),
+                        React.DOM.div( {style:{ display : (this.state.autocompletes.length > 0 ? 'block' : 'none'),
+                                    height: 'auto',
+                                    position: 'absolute',
+                                    bottom: '100%',
+                                    left: 0,
+                                    right: 0,
+                                    background: 'white',
+                                    border: 'solid 1px #aaa'
+                                    }}, 
+                            _.map(this.state.autocompletes, function(auto) {
+                                return React.DOM.div( {style:{ display: 'block', padding: '0.4em 0.3em', cursor: 'pointer' },
+                                            onClick:this.fireOne.bind(this, auto),
+                                            key:auto}, 
+                                    auto
+                                    )
+                            }.bind(this))
+                        )
+                    ),
+                    React.DOM.div( {className:"small-2 columns"}, 
+                        React.DOM.input( {type:"submit", className:"button postfix", value:"Add"} )
+                    )
+                )
+            ));
+    }
+});
---
+++ build/main.js
@@ -0,0 +1,21 @@
+/** @jsx React.DOM */
+(function() {
+    var hash = window.location.hash.replace(/^#/, '');
+
+    if (!hash) {
+        alert('Please load this page with the Firebase URL in the hash fragment.');
+        return;
+    }
+
+    var db = new Firebase(hash);
+
+    React.renderComponent(
+        React.DOM.div( {style:{ position: 'absolute', top: 0, bottom: 0, right: 0, left: 0 }}, 
+            React.DOM.div( {style:{ overflow: 'scroll', position: 'absolute', top: 0, bottom: 80, left: 0, right: 0 }}, 
+                ShoppingList( {firebase:db} )
+            ),
+            React.DOM.div( {style:{ position: 'absolute', bottom: 0, height: 80, left: 0, right: 0 }}, 
+                InputBox(null )
+            )
+        ), document.body);
+}());
