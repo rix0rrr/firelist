98edb59f2fdeba543b011e66865ae8dbc40e8c45
Rico Huijbers <rix0rrr@gmail.com>
2014-07-21 23:55:50 +0200

    Closer to bottom looks better on mobile

--- build/main.js
+++ build/main.js
@@ -11,10 +11,10 @@
 
     React.renderComponent(
         React.DOM.div( {style:{ position: 'absolute', top: 0, bottom: 0, right: 0, left: 0 }}, 
-            React.DOM.div( {style:{ overflow: 'scroll', position: 'absolute', top: 0, bottom: 80, left: 0, right: 0 }}, 
+            React.DOM.div( {style:{ overflow: 'scroll', position: 'absolute', top: 0, bottom: 60, left: 0, right: 0 }}, 
                 ShoppingList( {firebase:db} )
             ),
-            React.DOM.div( {style:{ position: 'absolute', bottom: 0, height: 80, left: 0, right: 0 }}, 
+            React.DOM.div( {style:{ position: 'absolute', bottom: 0, height: 60, left: 0, right: 0 }}, 
                 InputBox(null )
             )
         ), document.body);
--- jsx/main.js
+++ jsx/main.js
@@ -11,10 +11,10 @@
 
     React.renderComponent(
         <div style={{ position: 'absolute', top: 0, bottom: 0, right: 0, left: 0 }}>
-            <div style={{ overflow: 'scroll', position: 'absolute', top: 0, bottom: 80, left: 0, right: 0 }}>
+            <div style={{ overflow: 'scroll', position: 'absolute', top: 0, bottom: 60, left: 0, right: 0 }}>
                 <ShoppingList firebase={db} />
             </div>
-            <div style={{ position: 'absolute', bottom: 0, height: 80, left: 0, right: 0 }}>
+            <div style={{ position: 'absolute', bottom: 0, height: 60, left: 0, right: 0 }}>
                 <InputBox />
             </div>
         </div>, document.body);
